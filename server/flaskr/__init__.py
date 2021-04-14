bb668460d4a48c4afd8f29d264f401cfc5ec0a97
koutaro-masaki <koutaro.masaki@glucose.jp>
2021-04-14 17:33:45 +0900

    /testにPOSTとGETを実装してみた

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -1,27 +1,46 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, Markup, abort, jsonify
+
+# 簡易的にlistを使ってDBの代わりにする
+queue = []
 
 
 def create_app(test_config=None):
+    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
+        # load the test config if passed in
         app.config.from_mapping(test_config)
 
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
+    # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    @app.route('/test/<id>', methods=['GET', 'POST'])
+    def test(id):
+        if request.method == 'GET':
+            return jsonify({'id': int(id), 'query': request.args.get('query', ''), 'queue': queue})
+        elif request.method == 'POST':
+            json = request.get_json()
+            queue.append(json)
+            return json
+        else:
+            return abort(400)
 
     return app
