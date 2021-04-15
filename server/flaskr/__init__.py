af0c19397bcc7a49458242a4a31ab8a734b9e2b4
koutaro-masaki <koutaro.masaki@glucose.jp>
2021-04-15 11:50:47 +0900

    APIを実装しました

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -1,9 +1,10 @@
 import os
+import click
 
 from flask import Flask, request, Markup, abort, jsonify
-
-# 簡易的にlistを使ってDBの代わりにする
-queue = []
+from flask.cli import with_appcontext
+from .models.work import Work
+from .db import db, init_db
 
 
 def create_app(test_config=None):
@@ -11,8 +12,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        JSON_AS_ASCII=False
     )
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskr.sqlite'
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -27,20 +29,91 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    init_db(app)
+
     # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
-    @app.route('/test/<id>', methods=['GET', 'POST'])
-    def test(id):
+    # APIテスト
+    @app.route('/work/<id>', methods=['GET', 'PUT'])
+    def get_or_put(id):
+        work = db.session.query(Work).get(id)
+        if work is None:
+            abort(404)
+
         if request.method == 'GET':
-            return jsonify({'id': int(id), 'query': request.args.get('query', ''), 'queue': queue})
-        elif request.method == 'POST':
+            return jsonify({
+                'html': work.html,
+                'css': work.css,
+                'javascript': work.javascript
+            }), 200
+        elif request.method == 'PUT':
             json = request.get_json()
-            queue.append(json)
-            return json
+
+            if 'html' not in json:
+                return jsonify({
+                    'error': '不正なパラメータ',
+                    'message': "'html'フィールドが定義されていません"
+                }), 400
+            if 'css' not in json:
+                return jsonify({
+                    'error': '不正なパラメータ',
+                    'message': "'css'フィールドが定義されていません"
+                }), 400
+            if 'javascript' not in json:
+                return jsonify({
+                    'error': '不正なパラメータ',
+                    'message': "'javascript'フィールドが定義されていません"
+                }), 400
+
+            work.html = json['html']
+            work.css = json['css']
+            work.javascript = json['javascript']
+
+            # ここの処理で例外が出る可能性？
+            db.session.add(work)
+            db.session.commit()
+            db.session.close()
+            return jsonify({}), 200
         else:
             return abort(400)
+
+    @app.route('/work', methods=['POST'])
+    def post():
+        if request.method == 'POST':
+            json = request.get_json()
+
+            if 'html' not in json:
+                return jsonify({
+                    'error': '不正なパラメータ',
+                    'message': "'html'フィールドが定義されていません"
+                }), 400
+            if 'css' not in json:
+                return jsonify({
+                    'error': '不正なパラメータ',
+                    'message': "'css'フィールドが定義されていません"
+                }), 400
+            if 'javascript' not in json:
+                return jsonify({
+                    'error': '不正なパラメータ',
+                    'message': "'javascript'フィールドが定義されていません"
+                }), 400
+
+            work = Work(html=json['html'], css=json['css'],
+                        javascript=json['javascript'])
+
+            # ここの処理で例外が出る可能性？
+            db.session.add(work)
+            db.session.flush()
+            db.session.commit()
+
+            # sessionを閉じるとデータが取得できなくなるのでこのタイミングで格納しておく
+            result = {
+                'id': work.id
+            }
+            db.session.close()
+            return jsonify(result), 200
 
     return app
--- server/flaskr/db.py
+++ server/flaskr/db.py
@@ -1,24 +1,8 @@
-import sqlite3
-
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+
+db = SQLAlchemy()
 
 
-def get_db(app):
-    if 'db' not in g:
-        g.db = SQLAlchemy(app)
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
+def init_db(app):
+    db.init_app(app)
